b44d03076d0f1edd7ab13aa12db445fe3174053a
s26234-pj <s26234@pjwstk.edu.pl>
2023-01-27 18:04:51 +0100

    Initial commit

---
+++ src/main/java/com/mja/shopee__s26234/ShopeeS26234Application.java
@@ -0,0 +1,13 @@
+package com.mja.shopee__s26234;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeS26234Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeS26234Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mja/shopee__s26234/ShopeeS26234ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mja.shopee__s26234;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeS26234ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
